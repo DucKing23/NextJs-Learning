76643b1c4a9a539db2ff6395b5df98b6831f6868
Hoang Duc <minhduchoang2303@gmail.com>
2025-03-13 16:34:21 +0700

    BT boostrap

---
+++ src/components/app.footer.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center mb-0 bg-light py-3 fw-bold">
+      Hoàng Lê Minh Đức NEXTJS
+    </div>
+  );
+};
+
+export default Footer;
