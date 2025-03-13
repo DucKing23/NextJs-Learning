ee20b013367feaed25798dd29355e5d138f02b0d
Hoang Duc <minhduchoang2303@gmail.com>
2025-03-13 15:19:01 +0700

    Sử dụng css module cơ bản

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
+// sử dụng link để điều hướng trang
+// sử dụng event sử dụng useRouter
 const Duc = () => {
-  return <div></div>;
+  const handlebtn = () => {
+    router.push("/");
+  };
+
+  const router = useRouter();
+
+  return (
+    <>
+      <div>Facebook day</div>
+      <button onClick={() => handlebtn()}>abc</button>
+    </>
+  );
 };
 
 export default Duc;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import x from "@/styles/app.module.css";
+import y from "@/styles/red1.module.css";
 
 // Sử dụng thẻ Link để điều hướng trang giúp trang không bị load, làm tăng trải nghiệm người dùng
 export default function Home() {
   return (
     <>
       <ul>
-        <li>
+        <li className={x["red"]}>
           <Link href="./admin">Facebook</Link>
         </li>
-        <li>
+        <li className={y["red"]}>
           <Link href="./youtube">Youtube</Link>
         </li>
       </ul>
